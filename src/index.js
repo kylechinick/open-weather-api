b6c134b21967704b6005cb458375fd544670cb12
Kyle Chinick <kylechinick@icloud.com>
2022-06-27 14:04:35 -0700

    Add support for zipCode button click-event handler
    Co-authored-by: Caroline Cerussi <caroceru@gmail.com>
    Co-authored-by: Alex McKnight <alex.mcknight11@gmail.com>

--- src/index.js
+++ src/index.js
@@ -11,8 +11,7 @@ $(document).ready(function () {
     $('#state').val('');
 
     let request = new XMLHttpRequest();
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}, ${state}&appid=${process.env.API_KEY}&units=imperial`;
-
+    const url1 = `https://api.openweathermap.org/data/2.5/weather?q=${city}, ${state}&appid=${process.env.API_KEY}&units=imperial`;
     request.onreadystatechange = function () {
       if (this.readyState === 4 && this.status === 200) {
         const response = JSON.parse(this.responseText);
@@ -20,7 +19,7 @@ $(document).ready(function () {
       }
     };
 
-    request.open('GET', url, true);
+    request.open('GET', url1, true);
     request.send();
 
     function getElements(response) {
@@ -34,5 +33,36 @@ $(document).ready(function () {
         `the cloudiness in ${city} is  ${response.clouds.all}%`
       );
     }
+  });
+
+  $('#weatherZip').click(function () {
+    $('#weatherZip').click(function () {
+      const zipCode = $('#zipCode').val();
+      $('#zipCode').val('');
+
+      let request = new XMLHttpRequest();
+      const url2 = `https://api.openweathermap.org/data/2.5/weather?q=${zipCode},us&appid=${process.env.API_KEY}&units=imperial`;
+      request.onreadystatechange = function () {
+        if (this.readyState === 4 && this.status === 200) {
+          const response = JSON.parse(this.responseText);
+          getElements(response);
+        }
+      };
+
+      request.open('GET', url2, true);
+      request.send();
+
+      function getElements(response) {
+        $('.showHumidity').text(
+          `The humidity in ${zipCode} is ${response.main.humidity}%`
+        );
+        $('.showTemp').text(
+          `The temperature in Fahrenheit is ${response.main.temp} degrees.`
+        );
+        $('.showClouds').text(
+          `the cloudiness in ${zipCode} is  ${response.clouds.all}%`
+        );
+      }
+    });
   });
 });
